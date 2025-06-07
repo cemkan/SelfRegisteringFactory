1f26b10e4d80744926db470d7db58c14440387c8
Template Janitor <template.janitor@example.com>
2025-06-07 12:13:18 +0000

    Cleanup template and initialize repository

--- cmake/InterproceduralOptimization.cmake
+++ cmake/InterproceduralOptimization.cmake
@@ -1,4 +1,4 @@
-macro(myproject_enable_ipo)
+macro(SelfRegisteringFactory_enable_ipo)
   include(CheckIPOSupported)
   check_ipo_supported(RESULT result OUTPUT output)
   if(result)
--- src/sample_library/sample_library.cpp
+++ src/sample_library/sample_library.cpp
@@ -1,4 +1,4 @@
-#include <myproject/sample_library.hpp>
+#include <SelfRegisteringFactory/sample_library.hpp>
 
 int factorial(int input) noexcept
 {
